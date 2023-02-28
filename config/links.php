b7e9deeef074f8fef6fa9ba3a77802c3f18e99e4
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-28 16:42:46 +0100

    added file config

---
+++ config/links.php
@@ -0,0 +1,24 @@
+<?php  
+
+return [
+    [
+        "image" => "resources/images/buy-comics-digital-comics.png",
+        "alt" => "Digital Comics",
+    ],
+    [
+        "image" => "resources/images/buy-comics-merchandise.png",
+        "alt" => "Dc Merchandise"
+    ],
+    [
+        "image" => "resources/images/buy-comics-subscriptions.png",
+        "alt" => "Subscription"
+    ],
+    [
+        "image" => "resources/images/buy-comics-shop-locator.png",
+        "alt" => "Comic Shop Locator"
+    ],
+    [
+        "image" => "resources/images/buy-dc-power-visa.svg",
+        "alt" => "Dc Power Visa"
+    ],
+];
---
+++ config/linksfooter.php
@@ -0,0 +1,24 @@
+<?php  
+
+return [
+    [
+        "image" => "resources/images/footer-facebook.png",
+        "alt" => "Facebook",
+    ],
+    [
+        "image" => "resources/images/footer-periscope.png",
+        "alt" => "Periscope"
+    ],
+    [
+        "image" => "resources/images/footer-pinterest.png",
+        "alt" => "Pinterest"
+    ],
+    [
+        "image" => "resources/images/footer-twitter.png",
+        "alt" => "Twitter"
+    ],
+    [
+        "image" => "resources/images/footer-youtube.png",
+        "alt" => "Youtube"
+    ],
+];
